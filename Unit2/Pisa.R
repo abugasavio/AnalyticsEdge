aeac079054db486f3c96af9babd53c51d1a6054b
savioabuga <savioabuga@gmail.com>
2016-04-21 18:53:35 +0300

    releveling()
    relevel
    The levels of a factor are re-ordered so that the level specified by
    ref is  first and the others are moved down. This is useful for
    contr.treatment
    contrasts which take the first level as the reference.

--- Unit2/Pisa.R
+++ Unit2/Pisa.R
@@ -15,3 +15,30 @@ names(pisaTest)
 tapply(pisaTest$readingScore, pisaTest$male, mean)
 
 # Missing data
+
+summary(pisaTrain)
+
+# Remove missing data
+pisaTrain = na.omit(pisaTrain)
+nrow(pisaTrain)
+
+pisaTest = na.omit(pisaTest)
+
+nrow(pisaTest)
+
+# Factor Variables
+str(pisaTrain)
+
+levels(pisaTrain$raceeth)
+
+# relevel
+# The levels of a factor are re-ordered so that the level specified by ref is 
+# first and the others are moved down. This is useful for contr.treatment 
+# contrasts which take the first level as the reference.
+
+pisaTrain$raceeth = relevel(pisaTrain$raceeth, "White")
+
+pisaTest$raceeth = relevel(pisaTest$raceeth, "White")
+
+levels(pisaTrain$raceeth)
+
