4c14c6d3c69be3328effe600ac68b8e89a113b56
savioabuga <savioabuga@gmail.com>
2016-04-24 10:43:22 +0300

    Done with unit to compulsory homework

--- Unit2/FluEpidemics.R
+++ Unit2/FluEpidemics.R
@@ -35,4 +35,46 @@ FluTest$Predictions = PredTest1
 
 FluTest[FluTest$Week == '2012-03-11 - 2012-03-17',]
 
-# 
+# Calculating the errors
+SSE = sum((FluTest$Predictions - FluTest$ILI)^2)
+
+RMSE = sqrt(SSE/nrow(FluTest))
+RMSE
+
+# Using time lag
+library("zoo")
+
+ILILag2 = lag(zoo(FluTrain$ILI), -2, na.pad = TRUE)
+
+FluTrain$ILILag2 = coredata(ILILag2)
+
+FluTrain
+
+summary(FluTrain$ILILag2)
+
+plot(log(FluTrain$ILILag2), log(FluTrain$ILI))
+
+# Training ILI on ILILag2
+FluTrend2 = lm(log(FluTrain$ILI) ~ log(ILILag2) + Queries, data=FluTrain)
+summary(FluTrend2)
+
+# Adding ILLLag2 to the FlueTest
+FluTest$ILILag2 = coredata(lag(zoo(FluTest$ILI), -2, na.pad = TRUE))
+FluTest$ILILag2
+names(FluTrain)
+nrow(FluTest)
+
+Second = FluTrain[nrow(FluTrain), c('ILI')]
+First = FluTrain[nrow(FluTrain) - 1, c('ILI')]
+
+
+FluTest[FluTest$Week == "2012-01-08 - 2012-01-14", "ILILag2"] = Second
+FluTest[FluTest$Week == "2012-01-01 - 2012-01-07", "ILILag2"] = First
+
+
+FluTest$Predictions2 = exp(predict(FluTrend2, newdata = FluTest))
+
+FluTest
+SSE = sum((FluTest$Predictions2 - FluTest$ILI)^2)
+RMSE = sqrt(SSE/nrow(FluTest))
+RMSE
