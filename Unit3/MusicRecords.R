e8c66874178d3163d930291fa16482d72da35222
savioabuga <savioabuga@gmail.com>
2016-05-02 11:24:53 +0300

    analysing music data

--- Unit3/MusicRecords.R
+++ Unit3/MusicRecords.R
@@ -1,1 +1,9 @@
 setwd("/Users/savioabuga/Documents/Study/AnalyticsEdge/")
+
+songs = read.csv("Unit3/Data/songs.csv")
+str(songs)
+songs_2010 = subset(songs, year == "2010")
+str(songs_2010)
+
+michael_jackson = subset(songs, artistname == "Michael Jackson")
+michael_jackson[c["songtitle",]]
