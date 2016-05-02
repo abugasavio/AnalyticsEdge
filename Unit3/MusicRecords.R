91921b611e1fa40ae739b79567e4d8331a813054
savioabuga <savioabuga@gmail.com>
2016-05-02 11:41:51 +0300

    Understanding the music record data

--- Unit3/MusicRecords.R
+++ Unit3/MusicRecords.R
@@ -6,4 +6,21 @@ songs_2010 = subset(songs, year == "2010")
 str(songs_2010)
 
 michael_jackson = subset(songs, artistname == "Michael Jackson")
-michael_jackson[c["songtitle",]]
+michael_jackson[c("songtitle")]
+
+# Michael Jackson's songs that made it to the 10 billboard chart
+top_10_michael_jackson = subset(michael_jackson, Top10 == 1)
+str(top_10_michael_jackson)
+top_10_michael_jackson[c("songtitle")]
+
+factor(songs$timesignature)
+
+table(songs$timesignature)
+
+# song with the highest tempo
+songs$songtitle[which.max(songs$tempo)]
+
+# creating a logistic regression model
+SongsTrain = subset(songs, year <= 2009)
+SongsTest = subset(songs, year == 2010)
+
